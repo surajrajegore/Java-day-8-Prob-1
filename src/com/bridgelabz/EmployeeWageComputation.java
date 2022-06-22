3f56bbe0e3b65668e27669e4b348592f4830002f
surajrajegore <rajegoresuraj2880@gmail.com>
2022-06-22 19:04:31 +0530

    Added:Welcome Messege in Master

---
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+public class EmployeeWageComputation {
+    public static void main(String[] args)
+    {
+        getWelcomeMsg();
+    }
+    public static void getWelcomeMsg()
+    {
+        System.out.println("Welcome to EmployeeWage Computation...");
+    }
+}
